a712ef0d6bfa59192a0078c1311b7f9d8e282cd8
maendeleoperry <maendeleoperry@hotmail.com>
2019-08-01 16:23:18 +0000

    made some adjustments

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
   root 'quotes#index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'about', to: 'quotes#about'
+  resources :quotes
 end
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
